cf7d0f343dea7a1e3f4c0a2d1780410988391e3c
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-07-04 12:54:32 +0300

    Implement selection

--- tmux_attach.py
+++ tmux_attach.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from __future__ import annotations
+import time
+import string
 import libtmux
 import libtmux.server
 
@@ -7,20 +9,62 @@ import libtmux.server
 __version__ = "0.1.0"
 
 
+def create_new_session(server: libtmux.Server, session_name="0"):
+    server.new_session(session_name=session_name)
+    return session_name
+
+
 # TODO: replace try-except with port check
 def main() -> int:
     server = libtmux.Server()
-    session_name: str
+    session_name: str = ""
+
     try:
-        session = server.list_sessions()[0]
-        session_name = session.get("session_name")
+        sessions = server.list_sessions() 
+
+        print("Detected several sessions")
+        print(f"[q]: Quit selection")
+        print(f"[n]: Create new session")
+
+        for idx, session in enumerate(sessions):
+            session_name = session.get("session_name")
+            print(f"[{idx}]: {session_name}")
+
+        while True:
+            selection = input("Select option: ")
+
+            if selection not in string.digits and selection not in  "nq":
+                print("You must enter digit or 'n' or 'q'")
+                continue
+            
+            if selection not in 'nq' and int(selection) not in range(len(sessions)):
+                print("Input digit must be in range of list")
+                continue
+
+            if selection == 'n':
+                session_name = create_new_session(server, session_name=input("Name new session: "))
+            elif selection == 'q':
+                print("Quiting selection...")
+                return 0
+            else:
+                session_name = sessions[int(selection)].get("session_name")
+            break
+
     except libtmux.exc.LibTmuxException as e:
-        session_name = "0"
-        server.new_session(session_name=session_name)
+        # NOTE(gr3yknigh1): Looks a little bit tricky
+        # if no server is running (no session found), 
+        # then `libtmux.exc.LibTmuxException` will raised. 
+        # Then we just creating new session here.
+        session_name = create_new_session(server)
+
     server.attach_session(session_name)
     return 0
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    try:
+        raise SystemExit(main())
+    except KeyboardInterrupt:
+        print("Exiting after KeyboardInterrupt")
+        raise SystemExit(1)
 
