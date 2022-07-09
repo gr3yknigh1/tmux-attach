ca385b46eac2073e062d8f657a65272abf08f492
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-07-09 19:26:51 +0300

    Replace try catch with proc checking

--- tmux_session_selector.py
+++ tmux_session_selector.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 from __future__ import annotations
+import os
+import subprocess
+import psutil
 import time
 import string
 import libtmux
@@ -9,13 +12,31 @@ import libtmux.server
 __version__ = "1.0.2"
 
 
-def create_new_session(server: libtmux.Server, session_name="0"):
-    server.new_session(session_name=session_name)
-    return session_name
+TMUX_SERVER_PS_NAME = "tmux: server"
+TMUX_CLIENT_PS_NAME = "tmux: client"
+
+
+def is_proc_running(proc_name: str) -> bool:
+    for proc in psutil.process_iter():
+        if proc.name() == proc_name:
+            return True
+    return False
+
+
+def is_server_running() -> bool:
+    return is_proc_running(TMUX_SERVER_PS_NAME)
+
+
+def is_client_attached() -> bool:
+    return is_proc_running(TMUX_CLIENT_PS_NAME)
 
 
 # TODO: replace try-except with port check
 def main() -> int:
+    if is_client_attached():
+        print("Can't select when you attached to session")
+        return 1
+
     server = libtmux.Server()
     session_name: str = ""
     sessions = []
@@ -23,20 +44,11 @@ def main() -> int:
     print(f"[q]: Quit selection")
     print(f"[n]: Create new session")
 
-    try:
-        sessions.extend(server.list_sessions())
+    if is_server_running(): sessions.extend(server.list_sessions())
 
         for idx, session in enumerate(sessions):
             session_name = session.get("session_name")
             print(f"[{idx}]: {session_name}")
-
-    except libtmux.exc.LibTmuxException as e:
-        # NOTE(gr3yknigh1): Looks a little bit tricky
-        # if no server is running (no session found), 
-        # then `libtmux.exc.LibTmuxException` will raised. 
-        # Then we just creating new session here.
-        # session_name = create_new_session(server)
-        pass
 
     while True:
         selection = input("Select option: ")
@@ -50,13 +62,15 @@ def main() -> int:
             continue
 
         if selection == 'n':
-            session_name = create_new_session(server, session_name=input("Name new session: "))
+            session_name = input("Name new session: ")
+            server.new_session(session_name=session_name)
         elif selection == 'q':
             print("Quiting selection...")
             return 0
         else:
             session_name = sessions[int(selection)].get("session_name")
         break
+
 
     server.attach_session(session_name)
     return 0
